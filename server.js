550301b92dadbb08cf97c1a9d8219a04bf424617
Kai <kaiann.fletcher@mail.utoronto.ca>
2019-06-30 16:30:50 -0400

    updated mongo uri config and package.json

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://kaiann.fletcher@mail.utoronto.ca:JoyPhi48*@ds245387.mlab.com:45387/heroku_nft86grt");
 
 // Start the API server
 app.listen(PORT, function() {
